e6d83a93216e533cbc8ab3803b85806eae6e5044
BrokenShell <webmaster@sharpdesigndigital.com>
2022-04-08 16:10:26 -0700

    refactors js -> static/js/render.js, adds ui elements for graph selections

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
 
 APP = Flask(__name__)
 
 
-@APP.route("/")
+@APP.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+    x_axis = request.values.get("x_axis", "health")
+    y_axis = request.values.get("y_axis", "energy")
+    target = request.values.get("target", "rarity")
+
+    return render_template(
+        "index.html",
+        x_axis=x_axis,
+        y_axis=y_axis,
+        target=target,
+        options=["health", "energy", "sanity", "rarity", "level"],
+    )
 
 
 if __name__ == '__main__':
